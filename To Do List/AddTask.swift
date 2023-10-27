5d78be6d62424753db928e6742d05ab5c3688bc7
bahrov_m <37171644+bahrov@users.noreply.github.com>
2023-10-27 11:02:06 +0200

    Created basic functions

---
+++ To Do List/AddTask.swift
@@ -0,0 +1,18 @@
+//
+//  AddTask.swift
+//  To Do List
+//
+//  Created by Maksym on 2023-10-26.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTask()
+}
---
+++ To Do List/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  To Do List
+//
+//  Created by Maksym on 2023-10-27.
+//
+
+import Foundation
